576142967e93b964fc7436883e19ba8c264d4aa5
3030936 <3030936@30309-36>
2026-09-17 14:24:52 +0900

    自己紹介を追加

---
+++ src/main/java/com/example/demo/service/TanihataService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TanihataService {
+    public String getProfile() {
+        return "メンバーの谷畑です！趣味はAPEXです。";
+    }
+}
