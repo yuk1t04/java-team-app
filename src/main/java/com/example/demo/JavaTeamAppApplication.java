b7dab5d0c4b14e8a7828521297acc22192a21ca9
3030936 <3030936@30309-36>
2026-09-17 13:58:26 +0900

    プロジェクト始動

---
+++ src/main/java/com/example/demo/JavaTeamAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTeamAppApplication.class, args);
+	}
+
+}
